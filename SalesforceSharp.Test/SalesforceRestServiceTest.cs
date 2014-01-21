388400c0d5364c7998dff2569fee6ddeafbc8d22
Luis Rocha <lerocha@gmail.com>
2014-01-20 21:40:01 -0800

    Moving test project OAuth settings to config file

--- SalesforceSharp.Test/SalesforceRestServiceTest.cs
+++ SalesforceSharp.Test/SalesforceRestServiceTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Configuration;
 using NUnit.Framework;
 using SalesforceSharp.Responses;
 
@@ -7,9 +8,9 @@ namespace SalesforceSharp.Test
 	[TestFixture]
 	public class SalesforceRestServiceTest
 	{
-        const string ConsumerKey = "";
-        const string ConsumerSecret = "";
-        const string RefreshToken = "";
+        private string ConsumerKey = ConfigurationManager.AppSettings["ConsumerKey"];
+        private string ConsumerSecret = ConfigurationManager.AppSettings["ConsumerSecret"];
+        private string RefreshToken = ConfigurationManager.AppSettings["RefreshToken"];
 
 		[Test]
         public void SalesforceRestServiceConstructor()
