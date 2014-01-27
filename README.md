98140dd317575782e9634538cd99c5cccc160789
Luis Rocha <lerocha@gmail.com>
2014-01-26 23:33:11 -0800

    Updating README.md

--- README.md
+++ README.md
@@ -15,12 +15,47 @@ It implements a simple client for the [Salesforce REST API][1] using [RestSharp]
 var service = new SalesforceClient("ConsumerKey", "ConsumerSecret", "RefreshToken");
 
 // Execute a SOQL query
-List<Account> accounts = service.Query<Account>("SELECT id, name from Account");
+List<Contact> contacts = service.Query<Contact>("SELECT id, name from Contact");
 
 // Iterate through the records returned.
-foreach (Account account in accounts)
+foreach (Contact account in contacts)
 {
 	Console.WriteLine(account.Name);
+}
+
+// Create a new record
+var newContact = new
+{
+	FirstName = "John",
+	LastName = "Smith"
+};
+
+var id = service.Add<Contact>(newContact);
+
+// Read a record
+Contact contact = service.Get<Contact>(id);
+
+// Update a record
+var updateContact = new { Email = "jsmith@gmail.com" };
+service.Update<Contact>(updateContact, id);
+
+// Delete a record
+service.Delete<Contact>(id);
+
+// Error Handling
+try
+{
+	service.Add<Contact>(new { Name = "Read-only property" });
+}
+catch (SalesforceException e)
+{
+	Console.WriteLine("ErrorCode={0}; StatusCode={1}; Message={2}", e.ErrorCode, e.StatusCode, e.Message);
+	// Output:
+	// ErrorCode=INVALID_FIELD_FOR_INSERT_UPDATE; 
+	// StatusCode=BadRequest; 
+	// Message=Unable to create/update fields: Name. Please check the security settings of this field and verify that it is read/write for your profile or permission set.
+	
+	// TODO: handle exception
 }
 ```
   [1]: http://www.salesforce.com/us/developer/docs/api_rest/Content/resources_list.htm
--- SalesforceClient.Test/SalesforceClientTest.cs
+++ SalesforceClient.Test/SalesforceClientTest.cs
@@ -204,11 +204,11 @@ namespace SalesforceClient.Test
 	        var service = new SalesforceClient(ConsumerKey, ConsumerSecret, RefreshToken);
             
             // Act
-            Contact response = service.Get<Contact>(ContactId);
+            Contact contact = service.Get<Contact>(ContactId);
 	        
             // Assert
-            Assert.NotNull(response);
-            Assert.AreEqual(ContactId, response.Id);
+            Assert.NotNull(contact);
+            Assert.AreEqual(ContactId, contact.Id);
 	    }
 
 	    [Test]
@@ -249,6 +249,57 @@ namespace SalesforceClient.Test
             // Assert
             var exception = Assert.Throws<SalesforceException>(() => service.Update<Contact>(new { Description = Guid.NewGuid() }, "InvalidID"));
             Assert.AreEqual(HttpStatusCode.NotFound, exception.StatusCode);
+        }
+
+	    [Test]
+	    public void ReadmeTest()
+	    {
+            // Instantiate the client using a RefreshToken
+            var service = new SalesforceClient(ConsumerKey, ConsumerSecret, RefreshToken);
+
+            // Execute a SOQL query
+            List<Contact> contacts = service.Query<Contact>("SELECT id, name from Contact");
+
+            // Iterate through the records returned.
+            foreach (Contact account in contacts)
+            {
+                Console.WriteLine(account.Name);
+            }
+
+            // Create a new record
+            var newContact = new
+            {
+                FirstName = "John",
+                LastName = "Smith"
+            };
+
+            var id = service.Add<Contact>(newContact);
+
+            // Read a record
+            Contact contact = service.Get<Contact>(id);
+
+            // Update a record
+            var updateContact = new { Email = "jsmith@gmail.com" };
+            service.Update<Contact>(updateContact, id);
+
+            // Delete a record
+            service.Delete<Contact>(id);
+
+            // Error Handling
+	        try
+	        {
+                service.Add<Contact>(new { Name = "Read-only property" });
+            }
+	        catch (SalesforceException e)
+	        {
+                Console.WriteLine("ErrorCode={0}; StatusCode={1}; Message={2}", e.ErrorCode, e.StatusCode, e.Message);
+                // Output:
+                // ErrorCode=INVALID_FIELD_FOR_INSERT_UPDATE; 
+                // StatusCode=BadRequest; 
+                // Message=Unable to create/update fields: Name. Please check the security settings of this field and verify that it is read/write for your profile or permission set.
+	            
+                // TODO: handle exception
+	        }
         }
     }
 }
